55d9f43c498bfc85f1f357e0ff47cf738a35d601
972279393 <54498361+972279393@users.noreply.github.com>
2021-02-23 10:07:48 +0800

    Add files via upload

---
+++ login.py
@@ -0,0 +1,42 @@
+# !/usr/bin/python
+#coding=utf-8
+
+import requests
+import urllib
+import os
+import time
+
+URL = 'http://1.1.1.3/ac_portal/login.php'
+
+# username = '20190933t'
+# password = '@Chen2003'
+
+def login():
+    # 读取文本数据
+    f = open('data.txt','r')
+    A = f.readlines()
+
+    # 提交数据
+    data1 = {
+        'opr': 'pwdLogin',
+        'userName': A[0].strip(),
+        'pwd': A[1],
+        'auth_tag': '',
+        'rememberPwd': '1'
+    }
+
+    try:
+        result = requests.post(URL,data=data1)
+        if 'true' in result.text:
+            print('successful!')
+        else:
+            print('failed!')
+    except:
+        print('no')
+
+if __name__ == '__main__':
+    while True:
+        login()
+        # 5秒执行一次
+        time.sleep(5)
+        
